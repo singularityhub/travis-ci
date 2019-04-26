e5e107ac2667cac93e109d32ef47f86c1e933e6b
Vanessa Sochat <vsochat@stanford.edu>
2019-04-26 10:44:26 -0400

    try install as user
    Signed-off-by: Vanessa Sochat <vsochat@stanford.edu>

--- .travis/setup.sh
+++ .travis/setup.sh
@@ -6,17 +6,20 @@ sudo sed -i -e 's/^Defaults\tsecure_path.*$//' /etc/sudoers
 
 echo "Python Version:"
 python --version
-sudo pip install --upgrade pip 
-sudo pip install sregistry[all]
+pip install --user sregistry[all]
 sregistry version
 
 echo "sregistry Version:"
 
 # Install Singularity
 
+echo ${PWD}
+ls
 SINGULARITY_BASE="${GOPATH}/src/github.com/sylabs/singularity"
 export PATH="${GOPATH}/bin:${PATH}"
 cd ${SINGULARITY_BASE}
+echo ${PWD}
+ls
 ./mconfig -v -p /usr/local
 make -j `nproc 2>/dev/null || echo 1` -C ./builddir all
 sudo make -C ./builddir install
