c37eabf28c3d669ae4d36ba23316adee235553bc
Vanessa Sochat <vsochat@stanford.edu>
2019-04-26 10:38:15 -0400

    need sudo for install
    Signed-off-by: Vanessa Sochat <vsochat@stanford.edu>

--- .travis/setup.sh
+++ .travis/setup.sh
@@ -6,14 +6,17 @@ sudo sed -i -e 's/^Defaults\tsecure_path.*$//' /etc/sudoers
 
 echo "Python Version:"
 python --version
-pip install sregistry[all]
+sudo pip install --upgrade pip 
+sudo pip install sregistry[all]
 sregistry version
 
 echo "sregistry Version:"
 
 # Install Singularity
 
+SINGULARITY_BASE="${GOPATH}/src/github.com/sylabs/singularity"
 export PATH="${GOPATH}/bin:${PATH}"
+cd ${SINGULARITY_BASE}
 ./mconfig -v -p /usr/local
 make -j `nproc 2>/dev/null || echo 1` -C ./builddir all
 sudo make -C ./builddir install
