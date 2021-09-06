db4d81e38ceab0d3d179f142aed27bf8cb3f0a19
bartcharbon <bart.charbon@gmail.com>
2021-09-06 13:31:28 +0200

    and yet again

--- .travis/setup.sh
+++ .travis/setup.sh
@@ -6,14 +6,20 @@ sudo sed -i -e 's/^Defaults\tsecure_path.*$//' /etc/sudoers
 
 echo "Python Version:"
 python --version
+
 pip install --user sregistry[all]
+echo "sregistry Version:"
 sregistry version
 
-echo "sregistry Version:"
 
-curl -sfL https://install.goreleaser.com/github.com/golangci/golangci-lint.sh |
-sh -s -- -b $(go env GOPATH)/bin v1.13.0
-# Install Singularity
+export VERSION=1.13 OS=linux ARCH=amd64 && \
+    wget https://dl.google.com/go/go$VERSION.$OS-$ARCH.tar.gz && \
+    sudo tar -C /usr/local -xzvf go$VERSION.$OS-$ARCH.tar.gz && \
+    rm go$VERSION.$OS-$ARCH.tar.gz
+
+echo 'export GOPATH=${HOME}/go' >> ~/.bashrc && \
+    echo 'export PATH=/usr/local/go/bin:${PATH}:${GOPATH}/bin' >> ~/.bashrc && \
+    source ~/.bashrc
 
 echo "Go Version:"
 go version
