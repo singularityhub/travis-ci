47f2336fa94942cc77820e33a7fe5d104a12c098
bartcharbon <bart.charbon@gmail.com>
2021-09-06 13:12:11 +0200

    attempt to fix go

--- .travis/setup.sh
+++ .travis/setup.sh
@@ -11,12 +11,15 @@ sregistry version
 
 echo "sregistry Version:"
 
+curl -sfL https://install.goreleaser.com/github.com/golangci/golangci-lint.sh |
+sh -s -- -b $(go env GOPATH)/bin v1.21.0
 # Install Singularity
 
 export VERSION=3.7.2 && # adjust this as necessary \
     wget https://github.com/hpcng/singularity/releases/download/v${VERSION}/singularity-${VERSION}.tar.gz && \
     tar -xzf singularity-${VERSION}.tar.gz && \
     cd singularity
+
 
 ./mconfig && \
     make -C builddir && \
