83ec4df6a0aef1009d61d030d54cc53da8d0cb23
bartcharbon <bart.charbon@gmail.com>
2021-09-06 15:29:17 +0200

    ...

--- .travis/setup.sh
+++ .travis/setup.sh
@@ -3,21 +3,20 @@
 sudo sed -i -e 's/^Defaults\tsecure_path.*$//' /etc/sudoers
 
 # Check Python
+
 echo "Python Version:"
 python --version
-
 pip install --user sregistry[all]
-echo "sregistry Version:"
 sregistry version
 
-echo "Go Version:"
-go version
+echo "sregistry Version:"
+
+# Install Singularity
 
 export VERSION=3.7.2 && # adjust this as necessary \
     wget https://github.com/hpcng/singularity/releases/download/v${VERSION}/singularity-${VERSION}.tar.gz && \
     tar -xzf singularity-${VERSION}.tar.gz && \
     cd singularity
-
 
 ./mconfig && \
     make -C builddir && \
