37110164edf1eed95b5af26c31acf5ac59906d2d
bartcharbon <bart.charbon@gmail.com>
2021-09-06 13:45:45 +0200

    fix the yaml

--- .travis/setup.sh
+++ .travis/setup.sh
@@ -3,25 +3,12 @@
 sudo sed -i -e 's/^Defaults\tsecure_path.*$//' /etc/sudoers
 
 # Check Python
-
 echo "Python Version:"
 python --version
 
 pip install --user sregistry[all]
 echo "sregistry Version:"
 sregistry version
-
-sudo rm -rf /usr/local/go
-sudo rm /etc/paths.d/go
-
-export VERSION=1.13 OS=linux ARCH=amd64 && \
-    wget https://dl.google.com/go/go$VERSION.$OS-$ARCH.tar.gz && \
-    sudo tar -C /usr/local -xzvf go$VERSION.$OS-$ARCH.tar.gz && \
-    rm go$VERSION.$OS-$ARCH.tar.gz
-
-echo 'export GOPATH=${HOME}/go' >> ~/.bashrc && \
-    echo 'export PATH=/usr/local/go/bin:${PATH}:${GOPATH}/bin' >> ~/.bashrc && \
-    source ~/.bashrc
 
 echo "Go Version:"
 go version
